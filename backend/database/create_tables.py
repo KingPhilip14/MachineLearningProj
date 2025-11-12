32445b4e3409ff794a4693e28ab275642b0efb2e
Ian King <kingian404@gmail.com>
2025-11-12 01:23:56 -0600

    Changed plans to use SQL statements and use more tables in database. Must finish table creation queries

--- backend/database/create_tables.py
+++ backend/database/create_tables.py
@@ -4,25 +4,13 @@ def create_tables(conn):
     """
     cursor = conn.cursor()
 
-    # drop the tables if they exist already
-    drop_query = """
-                 DROP TABLE IF EXISTS account;
-                 DROP TABLE IF EXISTS team;
-                 DROP TABLE IF EXISTS pokemon_in_team;
-                 DROP TABLE IF EXISTS pokemon;
-                 DROP TABLE IF EXISTS pokemon_abilities;
-                 DROP TABLE IF EXISTS ability;
-                 """
-
-    cursor.execute(drop_query)
-
     # create the tables
     cursor.execute(create_account_table())
     cursor.execute(create_team_table())
-    # cursor.execute(create_pokemon_table())
-    # cursor.execute(create_pokemon_in_team_table())
-    # cursor.execute(create_ability_table())
-    # cursor.execute(create_pokemon_abilities_table())
+    cursor.execute(create_pokemon_table())
+    cursor.execute(create_pokemon_in_team_table())
+    cursor.execute(create_ability_table())
+    cursor.execute(create_pokemon_abilities_table())
     conn.commit()
 
 
@@ -40,52 +28,59 @@ def create_team_table() -> str:
     CREATE TABLE IF NOT EXISTS team(
         team_id INTEGER PRIMARY KEY SERIAL,
         FOREIGN KEY (account_id) REFERENCES account(account_id),
-        total_bst INTEGER NOT NULL,
+        team_name VARCHAR(30) NOT NULL,
+        time_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        generation varchar(20) NOT NULL,
     );
     """
 
-# def create_pokemon_table() -> str:
-#     return """
-#     CREATE TABLE IF NOT EXISTS pokemon(
-#         pokemon_id INTEGER PRIMARY KEY,
-#         nickname VARCHAR(30) NOT NULL,
-#         pokemon_name VARCHAR(30) NOT NULL,
-#         type_1 VARCHAR(10) NOT NULL,
-#         type_2 VARCHAR(10) NOT NULL,
-#         bst INTEGER NOT NULL,
-#         hp INTEGER NOT NULL,
-#         attack INTEGER NOT NULL,
-#         defense INTEGER NOT NULL,
-#         special_attack INTEGER NOT NULL,
-#         special_defense INTEGER NOT NULL,
-#         speed INTEGER NOT NULL,
-#         role VARCHAR(30 NOT NULL,
-#     );
-#     """
-#
-# def create_pokemon_in_team_table() -> str:
-#     return """
-#     CREATE TABLE IF NOT EXISTS pokemon_in_team(
-#         PRIMARY KEY (team_id, pokemon_id),
-#         FOREIGN KEY (team_id) REFERENCES team(team_id),
-#         FOREIGN KEY (pokemon_id) REFERENCES user(pokemon_id),
-#     );
-#     """
-#
-# def create_ability_table() -> str:
-#     return """
-#     CREATE TABLE IF NOT EXISTS ability(
-#         ability_id INTEGER PRIMARY KEY,
-#         ability_name VARCHAR(30) NOT NULL UNIQUE,
-#         effect_desc VARCHAR(150) NOT NULL,
-#     )
-#     """
-#
-# def create_pokemon_abilities_table() -> str:
-#     return """
-#     CREATE TABLE IF NOT EXISTS pokemon_abilities(
-#         PRIMARY KEY (pokemon_id, ability_id),
-#         FOREIGN KEY (pokemon_id) REFERENCES pokemon(pokemon_id),
-#         FOREIGN KEY (ability_id) REFERENCES ability(ability_id)
-#     );
-#     """
+def create_pokemon_table() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS pokemon(
+        pokemon_id INTEGER PRIMARY KEY,
+        FOREIGN KEY (learnset_id) REFERENCES learnset(learnset_id),
+        pokemon_name VARCHAR(30) NOT NULL,
+        pokemon_role VARCHAR(30 NOT NULL,
+        type_1 VARCHAR(10) NOT NULL,
+        type_2 VARCHAR(10) NOT NULL,
+        bst INTEGER NOT NULL,
+        hp INTEGER NOT NULL,
+        attack INTEGER NOT NULL,
+        defense INTEGER NOT NULL,
+        special_attack INTEGER NOT NULL,
+        special_defense INTEGER NOT NULL,
+        speed INTEGER NOT NULL,
+        weaknesses JSONB,
+        resistances JSONB,
+    );
+    """
+
+def create_ability_table() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS ability(
+        ability_id INTEGER PRIMARY KEY,
+        ability_name VARCHAR(30) NOT NULL UNIQUE,
+        effect_desc VARCHAR(150) NOT NULL,
+    )
+    """
+
+def create_pokemon_in_team_table() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS pokemon_in_team(
+        pit_id INTEGER PRIMARY KEY SERIAL,
+        FOREIGN KEY (team_id) REFERENCES team(team_id) NOT NULL,
+        FOREIGN KEY (pokemon_id) REFERENCES user(pokemon_id) NOT NULL,
+        FOREIGN KEY (chosen_ability_id) REFERENCES ability(ability_id) NOT NULL,
+        FOREIGN KEY (moveset_id) REFERENCES moveset(moveset_id) NOT NULL,
+        nickname VARCHAR(30) NOT NULL,
+    );
+    """
+
+def create_pokemon_abilities_table() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS pokemon_abilities(
+        PRIMARY KEY (pokemon_id, ability_id),
+        FOREIGN KEY (pokemon_id) REFERENCES pokemon(pokemon_id),
+        FOREIGN KEY (ability_id) REFERENCES ability(ability_id)
+    );
+    """
--- backend/database/db_utils.py
+++ backend/database/db_utils.py
@@ -1,6 +1,16 @@
 import os
 import psycopg2 as pg2
 from dotenv import load_dotenv
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# define the sqlalchemy engine for the database connection
+engine = db.create_engine('sqlite:///test.db', echo=True)
+
+# create the metadata object
+metadata = db.MetaData()
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
 
 def create_conn():
