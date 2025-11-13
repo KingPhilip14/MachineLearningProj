552f5adb6882f67f9c9e3709294ac28491fa3a53
KingPhilip14 <kingian404@gmail.com>
2025-11-12 19:27:12 -0600

    Made progress in SQL table creation

--- backend/database/create_tables.py
+++ backend/database/create_tables.py
@@ -10,7 +10,7 @@ def create_tables(conn):
     cursor.execute(create_pokemon_table())
     cursor.execute(create_pokemon_in_team_table())
     cursor.execute(create_ability_table())
-    cursor.execute(create_pokemon_abilities_table())
+    cursor.execute(create_pokemon_ability_table())
     conn.commit()
 
 
@@ -27,20 +27,52 @@ def create_team_table() -> str:
     return """
     CREATE TABLE IF NOT EXISTS team(
         team_id INTEGER PRIMARY KEY SERIAL,
-        FOREIGN KEY (account_id) REFERENCES account(account_id),
+        FOREIGN KEY (account_id) REFERENCES account(account_id) ON DELETE CASCADE,
         team_name VARCHAR(30) NOT NULL,
-        time_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         generation varchar(20) NOT NULL,
+        time_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        last_time_used TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        overlapping_weaknesses JSONB NOT NULL,
     );
+    """
+
+def create_movepool_table() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS movepool(
+        movepool_id INTEGER PRIMARY KEY SERIAL,
+        FOREIGN KEY (pokemon_id) REFERENCES pokemon(pokemon_id),
+    );
+    """
+
+def create_move_table() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS move(
+        move_id INTEGER PRIMARY KEY,
+        move_name VARCHAR(30) NOT NULL,
+        damage_class VARCHAR(30) NOT NULL,
+        power INTEGER,
+        accuracy INTEGER NOT NULL,
+        pp INTEGER NOT NULL,
+        priority INTEGER NOT NULL,
+    );
+    """
+
+def create_movepool_move_table() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS movepool_move(
+        PRIMARY KEY(movepool_id, move_id),
+        FOREIGN KEY (movepool_id) REFERENCES movepool(movepool_id),
+        FOREIGN KEY (move_id) REFERENCES move(move_id),
+    ); 
     """
 
 def create_pokemon_table() -> str:
     return """
     CREATE TABLE IF NOT EXISTS pokemon(
         pokemon_id INTEGER PRIMARY KEY,
-        FOREIGN KEY (learnset_id) REFERENCES learnset(learnset_id),
+        FOREIGN KEY (movepool_id) REFERENCES movepool(movepool_id),
         pokemon_name VARCHAR(30) NOT NULL,
-        pokemon_role VARCHAR(30 NOT NULL,
+        pokemon_role VARCHAR(30) NOT NULL,
         type_1 VARCHAR(10) NOT NULL,
         type_2 VARCHAR(10) NOT NULL,
         bst INTEGER NOT NULL,
@@ -50,6 +82,7 @@ def create_pokemon_table() -> str:
         special_attack INTEGER NOT NULL,
         special_defense INTEGER NOT NULL,
         speed INTEGER NOT NULL,
+        is_legend_or_mythical BOOLEAN NOT NULL,
         weaknesses JSONB,
         resistances JSONB,
     );
@@ -76,7 +109,7 @@ def create_pokemon_in_team_table() -> str:
     );
     """
 
-def create_pokemon_abilities_table() -> str:
+def create_pokemon_ability_table() -> str:
     return """
     CREATE TABLE IF NOT EXISTS pokemon_abilities(
         PRIMARY KEY (pokemon_id, ability_id),
@@ -84,3 +117,5 @@ def create_pokemon_abilities_table() -> str:
         FOREIGN KEY (ability_id) REFERENCES ability(ability_id)
     );
     """
+
+
