7092a7de87c7148cbbd06edb741646f9f9126d09
Ian King <kingian404@gmail.com>
2025-11-29 11:49:21 -0600

    Finished Login page skeleton

--- frontend/PAIGE/src/components/account-access/Login.tsx
+++ frontend/PAIGE/src/components/account-access/Login.tsx
@@ -1,9 +1,15 @@
 import "./AccountAccess.css";
 import { TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <div className={"page-container"}>
@@ -17,19 +23,68 @@ export default function Login() {
           <CardContent style={{ width: "80%vw", height: "80%vh" }}>
             <Typography
               variant={"h3"}
-              sx={{ textAlign: "center", color: "var(--text)" }}
+              sx={{
+                textAlign: "center",
+                color: "var(--text)",
+                margin: "40px 20px 20px 20px",
+              }}
             >
               Login
             </Typography>
+            <Typography variant="h6" style={{ margin: "40px 20px 40px 20px" }}>
+              Log in to PAIGE to access your saved teams!
+            </Typography>
 
-            <TextField required id="username" label="Username" />
+            <TextField
+              required
+              id="username"
+              label="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              style={{ margin: "30px 50px 30px 50px" }}
+              sx={{
+                fieldset: { borderColor: "var(--primary)" },
+              }}
+            />
             <TextField
               required
               id="outlined-password-input"
               label="Password"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               autoComplete="current-password"
+              style={{ margin: "30px 50px 30px 50px" }}
+              sx={{
+                fieldset: { borderColor: "var(--primary)" },
+              }}
             />
+            <div>
+              <Button
+                variant={"outlined"}
+                size={"large"}
+                sx={{
+                  borderColor: "var(--primary)",
+                  color: "var(--text)",
+                  margin: "40px 30px 40px 30px",
+                  padding: "15px 30px 15px 30px",
+                }}
+              >
+                Log in
+              </Button>
+            </div>
+
+            <div className={"dividing-line"}></div>
+
+            <div>
+              <Typography>
+                Don't have an account? {""}{" "}
+                <Link className={"link-style"} to={"/register"}>
+                  Register
+                </Link>{" "}
+                an account instead!
+              </Typography>
+            </div>
           </CardContent>
         </Card>
       </div>
