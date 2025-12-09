39246114b4bf70012b1fa08eb0357aa4bd1ac74d
Ian King <kingian404@gmail.com>
2025-12-09 13:46:10 -0600

    Moved API functionality from PokemonCards to GeneratedTeam

--- frontend/PAIGE/src/components/generated-team/GeneratedTeam.tsx
+++ frontend/PAIGE/src/components/generated-team/GeneratedTeam.tsx
@@ -3,38 +3,160 @@ import { SelectedGenCard } from "../SelectedGenCard.tsx";
 import LinearGradText from "../LinearGradText.tsx";
 import Button from "@mui/material/Button";
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { CircularProgress } from "@mui/material";
 
 export default function GeneratedTeam() {
+  interface PkmnTeam {
+    [pokemonName: string]: PkmnEntry;
+  }
+
+  interface PkmnEntry {
+    name: string;
+    nickname: string;
+    role: string;
+    role_description: string;
+    type_1: string;
+    type_2: string;
+    hp: number;
+    attack: number;
+    defense: number;
+    special_attack: number;
+    special_defense: number;
+    speed: number;
+    bst: number;
+    chosen_ability: string;
+    abilities: string[];
+  }
+
   const location = useLocation();
+  const [pkmnTeam, setPkmnTeam] = useState<PkmnTeam>({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [overlappingWeaknesses, setOverlappingWeaknesses] = useState<string[]>(
+    [],
+  );
   const { selectedGen, usingLittleCup, usingLegends, composition } =
     location.state || {};
 
+  useEffect(() => {
+    console.log("PokemonCards mounted");
+    async function fetchTeam() {
+      const data = await getGeneratedTeam();
+      if (!data || !Array.isArray(data) || data.length === 0) return;
+
+      setPkmnTeam(data[0]);
+
+      if (data[1]?.overlapping_weaknesses) {
+        setOverlappingWeaknesses(data[1].overlapping_weaknesses);
+      }
+    }
+
+    fetchTeam();
+  }, []);
+
+  async function getGeneratedTeam() {
+    const genFileName = selectedGen
+      ? selectedGen.replace(" ", "_").toLowerCase()
+      : "national";
+
+    setIsLoading(true);
+    try {
+      const response = await fetch("http://127.0.0.1:8000/generate-team", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          using_little_cup: usingLittleCup,
+          using_legends: usingLegends,
+          gen_file_name: genFileName,
+          composition: composition,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("A new team was not able to be generated.");
+      }
+
+      const data = await response.json();
+
+      return data || {};
+    } catch (error) {
+      console.error(error);
+
+      return {};
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <>
-      <div className={"page-container"}>
-        <SelectedGenCard selectedGen={selectedGen} backPage={"/team-config"} />
-        <LinearGradText text={"Generated Team"} />
+      {isLoading ? (
+        <div style={{ display: "flex", alignItems: "center", height: "50vh" }}>
+          <CircularProgress size="10rem" sx={{ color: "var(--primary)" }} />
+        </div>
+      ) : (
+        <div className={"page-container"}>
+          <SelectedGenCard
+            selectedGen={selectedGen}
+            backPage={"/team-config"}
+          />
+          <LinearGradText text={"Generated Team"} />
 
-        <PokemonCards
-          selectedGen={selectedGen}
-          usingLittleCup={usingLittleCup}
-          usingLegends={usingLegends}
-          composition={composition}
-        />
+          <PokemonCards
+            pkmnTeam={pkmnTeam}
+            updateAbility={(name, ability) =>
+              setPkmnTeam((prev) => ({
+                ...prev,
+                [name]: { ...prev[name], chosen_ability: ability },
+              }))
+            }
+          />
 
-        <Button
-          variant={"contained"}
-          size={"large"}
-          sx={{
-            backgroundColor: "var(--secondary)",
-            color: "var(--text)",
-            margin: "80px 0px 80px 0px",
-            minHeight: "50px",
-          }}
-        >
-          Save Team
-        </Button>
-      </div>
+          {!isLoading && (
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              sx={{ mt: 5 }}
+            >
+              <Typography sx={{ mb: 2, textAlign: "center" }}>
+                This team has the following overlapping weaknesses. Be mindful!
+              </Typography>
+              <Box
+                display="flex"
+                flexDirection="row"
+                justifyContent="center"
+                gap={3}
+              >
+                {overlappingWeaknesses.map((weakness) => (
+                  <img
+                    key={weakness}
+                    src={`/types_sprites/${weakness}.png`}
+                    alt={weakness}
+                  />
+                ))}
+              </Box>
+            </Box>
+          )}
+
+          <Button
+            variant={"contained"}
+            size={"large"}
+            sx={{
+              backgroundColor: "var(--secondary)",
+              color: "var(--text)",
+              margin: "80px 0px 80px 0px",
+              minHeight: "50px",
+            }}
+          >
+            Save Team
+          </Button>
+        </div>
+      )}
     </>
   );
 }
--- frontend/PAIGE/src/components/generated-team/PokemonCards.tsx
+++ frontend/PAIGE/src/components/generated-team/PokemonCards.tsx
@@ -7,122 +7,44 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import EditableTypography from "../EditableTypography.tsx";
 import AbilityMenu from "../AbilityMenu.tsx";
-import { useEffect, useState } from "react";
-import { CircularProgress } from "@mui/material";
 
-interface Props {
-  selectedGen: string;
-  usingLittleCup: string;
-  usingLegends: string;
-  composition: {
-    more_offensive: boolean;
-    more_defensive: boolean;
-    more_balanced: boolean;
-  };
+interface PokemonCardsProps {
+  pkmnTeam: PkmnTeam; // the fetched team
+  updateAbility: (name: string, ability: string) => void; // callback
+}
+
+interface PkmnEntry {
+  name: string;
+  nickname: string;
+  role: string;
+  role_description: string;
+  type_1: string;
+  type_2: string;
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+  bst: number;
+  chosen_ability: string;
+  abilities: string[];
+}
+
+interface PkmnTeam {
+  [pokemonName: string]: PkmnEntry;
 }
 
 export const PokemonCards = ({
-  selectedGen,
-  usingLittleCup,
-  usingLegends,
-  composition,
-}: Props) => {
-  interface PkmnEntry {
-    name: string;
-    nickname: string;
-    role: string;
-    role_description: string;
-    type_1: string;
-    type_2: string;
-    hp: number;
-    attack: number;
-    defense: number;
-    special_attack: number;
-    special_defense: number;
-    speed: number;
-    bst: number;
-    chosen_ability: string;
-    abilities: string[];
-  }
-
-  interface PkmnTeam {
-    [pokemonName: string]: PkmnEntry;
-  }
-
-  const [pkmnTeam, setPkmnTeam] = useState<PkmnTeam>({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [overlappingWeaknesses, setOverlappingWeaknesses] = useState<string[]>(
-    [],
-  );
-
-  useEffect(() => {
-    console.log("PokemonCards mounted");
-    async function fetchTeam() {
-      const data = await getGeneratedTeam();
-      if (!data || !Array.isArray(data) || data.length === 0) return;
-
-      setPkmnTeam(data[0]);
-
-      if (data[1]?.overlapping_weaknesses) {
-        setOverlappingWeaknesses(data[1].overlapping_weaknesses);
-      }
-    }
-
-    fetchTeam();
-  }, []);
-
+  pkmnTeam,
+  updateAbility,
+}: PokemonCardsProps) => {
   const PkmnCard = styled(Card)({
     display: "flex",
     borderRadius: "30px",
     color: "var(--text)",
     boxShadow: "3px 3px 8px var(--box-shadow)",
   });
-
-  const updateAbility = (name: string, ability: string) => {
-    setPkmnTeam((prev) => ({
-      ...prev,
-      [name]: {
-        ...prev[name],
-        chosen_ability: ability,
-      },
-    }));
-  };
-
-  async function getGeneratedTeam() {
-    const genFileName = selectedGen
-      ? selectedGen.replace(" ", "_").toLowerCase()
-      : "national";
-
-    setIsLoading(true);
-    try {
-      const response = await fetch("http://127.0.0.1:8000/generate-team", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          using_little_cup: usingLittleCup,
-          using_legends: usingLegends,
-          gen_file_name: genFileName,
-          composition: composition,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error("A new team was not able to be generated.");
-      }
-
-      const data = await response.json();
-
-      return data || {};
-    } catch (error) {
-      console.error(error);
-
-      return {};
-    } finally {
-      setIsLoading(false);
-    }
-  }
 
   function Types(type_1: string, type_2: string) {
     if (type_2 !== "") {
@@ -139,157 +61,119 @@ export const PokemonCards = ({
 
   return (
     <>
-      {isLoading ? (
-        <div style={{ display: "flex", alignItems: "center", height: "50vh" }}>
-          <CircularProgress size="10rem" sx={{ color: "var(--primary)" }} />
-        </div>
-      ) : (
-        <Box display="flex" justifyContent="center" width="100%">
-          <Grid
-            container
-            spacing={2}
-            justifyContent="center"
-            sx={{ maxWidth: "70rem", marginTop: "3rem" }}
-          >
-            {Object.values(pkmnTeam || {}).map((pkmn) => (
-              <Grid
-                size={{ xs: 6 }}
-                key={pkmn.name?.toLowerCase() || Math.random()}
+      <Box display="flex" justifyContent="center" width="100%">
+        <Grid
+          container
+          spacing={2}
+          justifyContent="center"
+          sx={{ maxWidth: "70rem", marginTop: "3rem" }}
+        >
+          {Object.values(pkmnTeam || {}).map((pkmn) => (
+            <Grid size={{ xs: 6 }} key={pkmn.name?.toLowerCase()}>
+              <PkmnCard
+                sx={{
+                  display: "flex",
+                  border: 1,
+                  borderStyle: "solid",
+                  borderColor: "var(--text)",
+                  width: "100%",
+                  height: "100%",
+                  p: 2,
+                }}
               >
-                <PkmnCard
+                <CardMedia
+                  className="sprite"
+                  component="img"
                   sx={{
-                    display: "flex",
-                    border: 1,
-                    borderStyle: "solid",
-                    borderColor: "var(--text)",
-                    width: "100%",
-                    height: "100%",
-                    p: 2,
+                    width: 151,
+                    objectFit: "contain",
                   }}
-                >
-                  <CardMedia
-                    className="sprite"
-                    component="img"
-                    sx={{
-                      width: 151,
-                      objectFit: "contain",
-                    }}
-                    image={`/pokemon_sprites/${pkmn.name.toLowerCase()}-sprite.png`}
-                    alt={pkmn.name + " sprite"}
-                  />
-                  <Box sx={{ display: "flex", flexDirection: "column" }}>
-                    <CardContent sx={{ flex: "1 0 auto" }}>
-                      <Box sx={{ display: "flex", alignItems: "center" }}>
-                        <EditableTypography
-                          givenText={pkmn.nickname}
-                          component="div"
-                          variant="h5"
-                          sx={{ marginRight: "5px" }}
-                        />
-                        {Types(pkmn.type_1, pkmn.type_2)}
-                      </Box>
-                      <Typography
-                        variant="subtitle1"
+                  image={`/pokemon_sprites/${pkmn.name.toLowerCase()}-sprite.png`}
+                  alt={pkmn.name + " sprite"}
+                />
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <CardContent sx={{ flex: "1 0 auto" }}>
+                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                      <EditableTypography
+                        givenText={pkmn.nickname}
                         component="div"
-                        sx={{ color: "text.secondary" }}
-                      >
-                        <em>
-                          <b>Role</b>: {pkmn.role}
-                        </em>
-                      </Typography>
-                      <Typography
-                        variant="subtitle1"
-                        component="div"
-                        sx={{ color: "text.secondary" }}
-                      >
-                        <em>
-                          <b>Chosen Ability</b>: {pkmn.chosen_ability}
-                        </em>
-                      </Typography>
-                      <Typography
-                        variant="subtitle1"
-                        component="div"
-                        sx={{ color: "text.secondary" }}
-                      >
-                        <em>
-                          <b>BST</b>: {pkmn.bst}
-                        </em>
-                      </Typography>
-                      <AbilityMenu
-                        menuText={"Change Ability"}
-                        pkmnName={pkmn.name}
-                        abilities={pkmn.abilities}
-                        onSelect={(ability) =>
-                          updateAbility(pkmn.name, ability)
-                        }
+                        variant="h5"
+                        sx={{ marginRight: "5px" }}
                       />
-                    </CardContent>
-                    <Box
-                      sx={{
-                        display: "grid",
-                        gridTemplateColumns: "1fr 1fr 1fr",
-                        rowGap: "5px",
-                        columnGap: "20px",
-                        alignItems: "center",
-                        pl: 1,
-                        pb: 1,
-                        margin: "5px",
-                      }}
-                    >
-                      {/*The order allows for it appear in the way desired*/}
-                      <Typography>
-                        <b>HP</b>: {pkmn.hp}
-                      </Typography>
-                      <Typography>
-                        <b>Attack</b>: {pkmn.attack}
-                      </Typography>
-                      <Typography>
-                        <b>Def</b>: {pkmn.defense}
-                      </Typography>
-                      <Typography>
-                        <b>Sp. Atk</b>: {pkmn.special_attack}
-                      </Typography>
-                      <Typography>
-                        <b>Sp. Def</b>: {pkmn.special_defense}
-                      </Typography>
-                      <Typography>
-                        <b>Speed</b>: {pkmn.speed}
-                      </Typography>
+                      {Types(pkmn.type_1, pkmn.type_2)}
                     </Box>
-                  </Box>
-                </PkmnCard>
-              </Grid>
-            ))}
-            {!isLoading && (
-              <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                sx={{ mt: 5 }}
-              >
-                <Typography sx={{ mb: 2, textAlign: "center" }}>
-                  This team has the following overlapping weaknesses. Be
-                  mindful!
-                </Typography>
-                <Box
-                  display="flex"
-                  flexDirection="row"
-                  justifyContent="center"
-                  gap={3}
-                >
-                  {overlappingWeaknesses.map((weakness) => (
-                    <img
-                      key={weakness}
-                      src={`/types_sprites/${weakness}.png`}
-                      alt={weakness}
+                    <Typography
+                      variant="subtitle1"
+                      component="div"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      <em>
+                        <b>Role</b>: {pkmn.role}
+                      </em>
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      component="div"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      <em>
+                        <b>Chosen Ability</b>: {pkmn.chosen_ability}
+                      </em>
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      component="div"
+                      sx={{ color: "text.secondary" }}
+                    >
+                      <em>
+                        <b>BST</b>: {pkmn.bst}
+                      </em>
+                    </Typography>
+                    <AbilityMenu
+                      menuText={"Change Ability"}
+                      pkmnName={pkmn.name}
+                      abilities={pkmn.abilities}
+                      onSelect={(ability) => updateAbility(pkmn.name, ability)}
                     />
-                  ))}
+                  </CardContent>
+                  <Box
+                    sx={{
+                      display: "grid",
+                      gridTemplateColumns: "1fr 1fr 1fr",
+                      rowGap: "5px",
+                      columnGap: "20px",
+                      alignItems: "center",
+                      pl: 1,
+                      pb: 1,
+                      margin: "5px",
+                    }}
+                  >
+                    {/*The order allows for it appear in the way desired*/}
+                    <Typography>
+                      <b>HP</b>: {pkmn.hp}
+                    </Typography>
+                    <Typography>
+                      <b>Attack</b>: {pkmn.attack}
+                    </Typography>
+                    <Typography>
+                      <b>Def</b>: {pkmn.defense}
+                    </Typography>
+                    <Typography>
+                      <b>Sp. Atk</b>: {pkmn.special_attack}
+                    </Typography>
+                    <Typography>
+                      <b>Sp. Def</b>: {pkmn.special_defense}
+                    </Typography>
+                    <Typography>
+                      <b>Speed</b>: {pkmn.speed}
+                    </Typography>
+                  </Box>
                 </Box>
-              </Box>
-            )}
-          </Grid>
-        </Box>
-      )}
+              </PkmnCard>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </>
   );
 };
